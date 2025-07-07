f938894999f6018f1a7a7f8da2e2144f6fab2106
nagiyu <otonagiyu@gmail.com>
2025-07-08 00:55:53 +0900

    fix: update Dockerfile and build script to correct paths for copying files

--- client/nextjs/build.sh
+++ client/nextjs/build.sh
@@ -2,7 +2,5 @@
 
 docker build \
   --build-arg PROCESS_ENV=local \
-  --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_ACCESS_KEY= \
-  --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_SECRET_ACCESS_KEY= \
-  --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_REGION= \
+  -f nextjs/Dockerfile \
   -t client-nextjs-template .
