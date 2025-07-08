9ca75ab6fba9877604674f96154c798197e7097c
nagiyu <otonagiyu@gmail.com>
2025-07-08 16:06:59 +0900

    fix: add NEXTAUTH_URL build argument to Dockerfile and related scripts

--- client/nextjs/build.sh
+++ client/nextjs/build.sh
@@ -5,5 +5,6 @@ docker build \
   --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_ACCESS_KEY= \
   --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_SECRET_ACCESS_KEY= \
   --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_REGION= \
+  --build-arg NEXTAUTH_URL= \
   -f nextjs/Dockerfile \
   -t client-nextjs-template .
