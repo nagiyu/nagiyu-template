6b92c7e9615984ac08cdab8d95f9d69616b444a2
nagiyu <153594832+nagiyu@users.noreply.github.com>
2025-07-08 18:41:31 +0900

    Delete client directory

--- client/nextjs/app/components/SignedInContent.tsx
+++
@@ -1,7 +0,0 @@
-import { Session } from 'next-auth';
-
-export default function SignedInContent({ session }: { session: Session }) {
-    return (
-        <div>サインイン: {JSON.stringify(session)}</div>
-    );
-}
