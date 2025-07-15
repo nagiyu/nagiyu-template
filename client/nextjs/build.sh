7033edbeb707d95dae92d229f6ad97191544ed93
nagiyu <153594832+nagiyu@users.noreply.github.com>
2025-07-15 13:07:20 +0900

    Update build.sh

--- client/nextjs/build.sh
+++ client/nextjs/build.sh
@@ -2,6 +2,7 @@
 
 docker build \
   --build-arg PROCESS_ENV=local \
+  --build-arg PROJECT_SECRET=ClientNextjsTemplate \
   --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_ACCESS_KEY= \
   --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_SECRET_ACCESS_KEY= \
   --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_REGION= \
