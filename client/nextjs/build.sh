bb3181cff441e98b9ac0e18894871aaf2195cd34
nagiyu <otonagiyu@gmail.com>
2025-07-16 11:28:44 +0900

    feat: initialize Next.js client with essential configurations and assets
    - Added package.json with dependencies and scripts for Next.js application.
    - Included SVG icons for UI components (file.svg, globe.svg, next.svg, window.svg, vercel.svg).
    - Added a service worker (sw.js) to handle push notifications.
    - Included a PNG asset (ponzu_square.png) for notification icons.
    - Created tsconfig.json for TypeScript configuration in the Next.js project.

---
+++ client/nextjs/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker build \
+  --build-arg PROCESS_ENV=local \
+  --build-arg PROJECT_SECRET=ClientNextjsTemplate \
+  --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_ACCESS_KEY= \
+  --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_SECRET_ACCESS_KEY= \
+  --build-arg CLIENT_NEXTJS_TEMPLATE_AWS_REGION= \
+  --build-arg NEXTAUTH_URL= \
+  -f nextjs/Dockerfile \
+  -t client-nextjs-template .
